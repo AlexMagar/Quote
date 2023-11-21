7d3b6421c7f6ee9d4779e66804a874495f6f836f
AlexMagar <lx.mgr07@gmail.com>
2023-11-21 23:37:16 +1100

    feat: debug

---
+++ src/component/FetchData/FetchData.tsx
@@ -0,0 +1,77 @@
+import { ReactElement, useEffect, useState } from "react"
+import Spinner from "../Spinner"
+
+export type TAdviceSlip = {
+    slip: {
+        id: number,
+        advice: string,
+    }
+}
+
+const FetchData = () => {
+
+    const [loading, setLoading] = useState<boolean>(false)
+    const [data, setData] = useState<TAdviceSlip | null>(null)
+    const [error, setError] = useState<string | null>(null)
+
+    const slip = data?.slip?.advice
+
+    //fetch data from API
+    const fetchHandler = async () => {
+        const abortController = new AbortController()
+        const signal = abortController.signal
+
+        setLoading(true)
+        setError(null)
+
+        await fetch("https://api.adviceslip.com/advice", {signal})
+        .then((response) => {
+            if(!response.ok){
+                throw new Error(`This is an HTTP error: The status is ${response.status}`)
+            }
+            return response.json();
+        })
+        .then((advice) => {
+            setLoading(false)
+            setError(null)
+            setData(advice)
+        }) 
+        .catch((error) => {
+            setError(error.message)
+            setData(null)
+            setLoading(false)
+        })
+        return () => abortController.abort()
+    }
+
+    useEffect(() => {
+        fetchHandler()
+    }, [])
+
+    const content = (
+        <>
+            {
+                loading ? (
+                    <Spinner />
+                ) : error ? (
+                    <p>Error: (error.message)</p>
+                ) : (
+                    <div>
+                        <p className="main-message">"{slip}"</p>
+                        <button
+                            onClick={fetchHandler}
+                            title={'Refresh the advice'}
+                            type={'button'}
+                        >
+                            Refresh
+                        </button>
+                    </div>
+                )
+            }
+        </>
+    )
+
+    return content
+}
+
+export default FetchData
